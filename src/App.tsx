e9f9bcbb2a6ceebf60e19fcf6b94f7b7a57b61c9
karlcode <karl.l1@hotmail.com>
2019-11-05 22:28:12 +1100

    Adding new hover reveal layout

--- src/App.tsx
+++ src/App.tsx
@@ -10,34 +10,44 @@ const list = {
 
 const item = {
     visible: { opacity: 1, x: 0 },
-    hidden: { opacity: 0, x: -100 }
+    hidden: { opacity: 0, x: -100 },
+    opened: { height: "100px" }
 };
 
 function App() {
+    const [isVisible, setVisible] = useState(false)
     return (
         <div className="App">
             <div className="menu">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>Karl</h1>
+                {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                <motion.div onMouseEnter={() => setVisible(true)}>
+                    <h1>Karl</h1>
+                </motion.div>
+                <motion.ul
+                    className="portfolio-links"
+                    initial="hidden"
+                    animate={{ opacity: isVisible ? "1" : 0}}
+                    variants={list}
+                >
+                    {[1, 2, 3].map(element => (
+                        <motion.li
+                            drag
+                            dragConstraints={{
+                                left: 0,
+                                right: 0,
+                                top: 0,
+                                bottom: 0
+                            }}
+                            dragElastic={0.9}
+                            variants={item}
+                            key={element}
+                            whileHover="opened"
+                        >
+                            {element}
+                        </motion.li>
+                    ))}
+                </motion.ul>
             </div>
-            <motion.ul
-                className="portfolio-links"
-                initial="hidden"
-                animate="visible"
-                variants={list}
-            >
-                {[1, 2, 3].map(element => (
-                    <motion.li
-                        drag
-                        dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-                        dragElastic={0.9}
-                        variants={item}
-                        key={element}
-                    >
-                        {element}
-                    </motion.li>
-                ))}
-            </motion.ul>
         </div>
     );
 }
